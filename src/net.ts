2ba6b3da62e2683b40e987a600de62bad721256c
Mike Prince <mike@mikeprince.com>
2025-03-26 17:14:49 -0700

    Removed unused net functions, up to 0.3.1

--- src/net.ts
+++ src/net.ts
@@ -6,25 +6,6 @@ import {
 
 import { inherits } from 'util'
 
-//
-// Errors are coded with an integer array.  The leftmost/first number
-// is the most significant, with each subsequent number having less
-// significance.
-//
-// The first number is designed to correspond to the major classes
-// of HTTP status codes:
-// 2 => 2xx, OK status codes
-// 4 => 4xx, Request failed due to incorrect client call
-// 5 => 5xx, Request failed because of a server problem
-//
-
-// Use this method when we DON'T have an Error object
-export function signalNotOk( req:Request, res:Response, code:number[], message:string, details:any ) {
-    const failure = { code, message, details };
-    log(req,failure);
-    res.status( errorCodeToStatusCode(code) ).json({failure});
-}
-
 function errorCodeToStatusCode( code: any ) {
     if( !code || !Array.isArray(code) )
         return 500;
@@ -84,11 +65,7 @@ function log( req: Request, failure: any, err?: any ) {
 }
 
 export function baseUrl( req: Request ) {
-    if( process.env.SERVICE_CALLBACK_BASE_URL )
-        return process.env.SERVICE_CALLBACK_BASE_URL;
-
-    const protocol = process.env.SERVICE_CALLBACK_HTTP_PROTOCOL || req.protocol;
-    return (protocol + "://" + req.get('host')).toLowerCase();
+    return (req.protocol + "://" + req.get('host')).toLowerCase();
 }
 
 export type AsyncMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<any>;
@@ -98,12 +75,4 @@ export const asyncHandler = (fn: AsyncMiddleware) => function( req: Request, res
     return Promise.resolve(fnReturn).catch( err => {
         signalError(req,res,err);
     });
-}
-
-export function sendBearer401( res: Response, reason: string ) {
-    res.statusCode = 401;
-    res.setHeader('WWW-Authenticate', 'Bearer realm="Agentic-Profile"');
-
-    const failure = { code:[4,1], message: 'Access Denied: ' + reason };
-    res.json({ failure });   
 }
